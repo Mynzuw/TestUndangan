4836ecb5221e21e994724541a1e4f0e25aca2f51
Miftah Adha <110320264+Mynzuw@users.noreply.github.com>
2024-06-27 19:02:05 +0700

    Update confetti.js

--- js/confetti.js
+++ js/confetti.js
@@ -1,1 +1,14 @@
 export const confetti = window.confetti;
+
+function launchConfetti() {
+    confetti({
+        particleCount: 200,
+        spread: 160,
+        origin: { y: 0.6 },
+        colors: ['#808080'] // Gray color
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    launchConfetti();
+});
